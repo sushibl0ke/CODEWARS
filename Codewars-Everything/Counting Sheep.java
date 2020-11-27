577b16373cd61e91bc130d094cc06484e2bbe78a
sushibl0ke <71844776+sushibl0ke@users.noreply.github.com>
2020-11-27 10:09:45 +0200

    Create Counting Sheep.java

---
+++ Codewars-Everything/Counting Sheep.java
@@ -0,0 +1,14 @@
+public class Counter {
+ public int countSheeps(Boolean[] arrayOfSheeps) {
+        int count = 0;
+        for (int i = 0; i < arrayOfSheeps.length; i++) {
+            if (arrayOfSheeps[i]==null||!arrayOfSheeps[i]){
+              count=count;
+            }
+            else {
+                count++;
+            }
+        }
+        return count;
+    }
+  }
