fdddf4bb2df94066b53cc487fa5137ef6797906f
sushibl0ke <71844776+sushibl0ke@users.noreply.github.com>
2020-11-27 10:55:06 +0200

    Create Calculate average.java

---
+++ Codewars-Everything/Calculate average.java
@@ -0,0 +1,11 @@
+public class Kata{
+  public static double find_average(int[] array){
+  double total = 0;
+
+        for(int i=0; i<array.length; i++){
+          total = total + array[i];
+        }
+        double average = total / array.length;
+        return(average);
+  }
+}
