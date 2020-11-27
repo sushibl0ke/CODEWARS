ddd6d4f7b5da5156f394e272d4a0ae601100b2d3
sushibl0ke <71844776+sushibl0ke@users.noreply.github.com>
2020-11-27 11:47:59 +0200

    Create Your order please.java

---
+++ Codewars-Everything/Your order please.java
@@ -0,0 +1,12 @@
+public class Order {
+    public static String order(String words) {
+        String[] strarr = words.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= strarr.length; i++) {
+            for (String str : strarr) {
+                if (str.contains(Integer.toString(i))) sb.append(str).append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+}
