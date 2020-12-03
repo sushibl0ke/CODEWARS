a81d2ecfbd319beaa7a305f0e22593f5eb486891
sushibl0ke <71844776+sushibl0ke@users.noreply.github.com>
2020-12-03 13:39:09 +0200

    Create WhatsYourPoison.java

---
+++ Codewars-01-December/WhatsYourPoison.java
@@ -0,0 +1,10 @@
+public class FindThePoison {
+
+    public int find(int[] rats) {
+      
+        int PLESHO = 0;
+        for (int faggot : rats) PLESHO += Math.pow(2, faggot);
+        return PLESHO;
+      
+    }
+}
